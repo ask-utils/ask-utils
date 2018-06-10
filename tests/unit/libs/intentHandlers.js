b99f2c26c2f0684782f3e708e6ba1dbb464f6cf3
hideokamoto <hide.okamoto@digitalcube.jp>
2018-06-10 15:06:25 +0900

    add supported display functions

--- tests/unit/libs/intentHandlers.js
+++ tests/unit/libs/intentHandlers.js
@@ -3,6 +3,47 @@ const utils = require('../../../libs/intentHandlers')
 const { handlerInput } = require('../../handlerInput')
 
 describe('libs/intentHandlers.js', () => {
+  describe('#supportsDisplay()', () => {
+    it('should return true when given from supported display', () => {
+      const result = utils.supportsDisplay(handlerInput)
+      assert.equal(result, true)
+    })
+    it('should return false when given from un-supported display', () => {
+      const unsupportedDisplayInput = handlerInput
+      unsupportedDisplayInput.requestEnvelope.context.System.device.supportedInterfaces = {
+        'AudioPlayer': {}
+      }
+      const result = utils.supportsDisplay(unsupportedDisplayInput)
+      assert.equal(result, false)
+    })
+    it('should return false when given invalid params', () => {
+      const result = utils.supportsDisplay({})
+      assert.equal(result, false)
+    })
+  })
+  describe('#getSupportedInterfaces()', () => {
+    it('should return valid supportedInterfaces', () => {
+      handlerInput.requestEnvelope.context.System.device.supportedInterfaces = {
+        'AudioPlayer': {},
+        'Display': {
+          'templateVersion': '1.0',
+          'markupVersion': '1.0'
+        }
+      }
+      const result = utils.getSupportedInterfaces(handlerInput)
+      assert.deepEqual(result, {
+        'AudioPlayer': {},
+        'Display': {
+          'templateVersion': '1.0',
+          'markupVersion': '1.0'
+        }
+      })
+    })
+    it('should return empty object when given invalid input', () => {
+      const result = utils.getSupportedInterfaces({})
+      assert.deepEqual(result, {})
+    })
+  })
   describe('#isHandledIntent()', () => {
     it('should return true when given matched intent name', () => {
       handlerInput.requestEnvelope.request.intent.name = 'HelloWorldIntent'

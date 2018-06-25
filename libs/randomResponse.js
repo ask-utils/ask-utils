8fca72be387155136c89dc10681778da864a5408
hideokamoto <hide.okamoto@digitalcube.jp>
2018-06-25 13:00:08 +0900

    some update

--- libs/randomResponse.js
+++ libs/randomResponse.js
@@ -1,3 +1,10 @@
+/**
+ * Create random message from lists of messages
+ *
+ * @param {array} messages - lists of message
+ * @return {string} - random message
+ * @since 0.1.0
+ **/
 const getRandomMessage = (messages) => {
   if (!messages || messages.length === 0) return ''
   const key = Math.floor(Math.random() * messages.length)

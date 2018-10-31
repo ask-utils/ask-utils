aed858584f008b5be639b9fa669a905c84e4c555
hideokamoto <hide.okamoto@digitalcube.jp>
2018-10-31 10:23:47 +0900

    fix

--- docs/scripts/custom.js
+++ docs/scripts/custom.js
@@ -1,6 +1,6 @@
 docsearch({ 
   apiKey: '6c5c02d787f0d00cb918b163f33af6af', 
   indexName: 'ask-utils', 
-  inputSelector: '### REPLACE ME ####', 
+  inputSelector: '#nav-search', 
   debug: false // Set debug to true if you want to inspect the dropdown 
 }); 

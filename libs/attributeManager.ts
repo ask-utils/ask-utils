558a568fbfdc24c38b73cb123e9c931e332e1fe3
hideokamoto <hide.okamoto@digitalcube.jp>
2019-02-28 23:25:20 +0900

    update type

--- libs/attributeManager.ts
+++ libs/attributeManager.ts
@@ -1,14 +1,15 @@
 import { HandlerInput } from 'ask-sdk'
-export const updateSessionAttributes = (handlerInput: HandlerInput, updateObject: {[attributeName: string]: string}): void => {
+
+export const updateSessionAttributes = (handlerInput: HandlerInput, updateObject: {[attributeName: string]: any}): void => {
     const atts = handlerInput.attributesManager.getSessionAttributes()
     handlerInput.attributesManager.setSessionAttributes(Object.assign(atts, updateObject))
 }
 
-export const getSessionAttributes = (handlerInput: HandlerInput): {[attributeName: string]: string} => {
+export const getSessionAttributes = (handlerInput: HandlerInput): {[attributeName: string]: any} => {
     return handlerInput.attributesManager.getSessionAttributes()
 }
 
-export const getSessionAttribute = (handlerInput: HandlerInput, attributeName: string): string => {
+export const getSessionAttribute = (handlerInput: HandlerInput, attributeName: string): any => {
     const atts = getSessionAttributes(handlerInput)
     return atts[attributeName]
 }

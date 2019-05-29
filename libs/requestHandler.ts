423048096dee0f1efc6dafaf900ddaf6c8f2f8d5
hideokamoto <hide.okamoto@digitalcube.jp>
2019-05-29 18:14:04 +0900

    add connection request

--- libs/requestHandler.ts
+++ libs/requestHandler.ts
@@ -1,5 +1,9 @@
 import { RequestHandler, HandlerInput } from 'ask-sdk-core'
-import { Response } from 'ask-sdk-model'
+import {
+    interfaces,
+    Request
+} from 'ask-sdk-model'
+
 export const cloneHandler = (cloneTarget: RequestHandler): RequestHandler => {
     return Object.assign({}, cloneTarget)
 }
@@ -11,4 +15,12 @@ interface MergeObjectType {
 
 export const mergeHandler = (cloneTarget: RequestHandler, mergeObject: MergeObjectType): RequestHandler => {
     return Object.assign({}, cloneTarget, mergeObject)
+}
+
+export const isSkillConnectionResponse = (request: Request): request is interfaces.connections.ConnectionsResponse => {
+    return request.type === 'Connections.Response'
+}
+
+export const isSkillConnectionRequest = (request: Request): request is interfaces.connections.ConnectionsRequest => {
+    return request.type === 'Connections.Request'
 }

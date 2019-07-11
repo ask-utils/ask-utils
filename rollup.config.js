1cc6ffd8584ad777ced6d467e4ddcfe80f2624e0
hideokamoto <hide.okamoto@digitalcube.jp>
2019-07-11 16:44:54 +0900

    update config files

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,18 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from 'rollup-plugin-typescript2'
 export default {
-  input: './libs/index.ts',
-  output: [{
-    file: './dist/index.js',
-    format: 'cjs'
-  }],
-  plugins: [
-    // nodeResolve({jsnext: true}),
-    // commonjs(),
-    // json(),
-    typescript({
-      tsconfigOverride: {
-        comppilerOptions: {
-          module: 'es2015',
-          moduleResolution: 'node'
-        }
-      }
-    })
-  ]
+    input: './libs/index.ts',
+    output: [{
+        file: './dist/index.js',
+        format: 'cjs'
+    }],
+    plugins: [
+        typescript({
+            tsconfigOverride: {
+                comppilerOptions: {
+                    module: 'es2015',
+                    moduleResolution: 'node'
+                }
+            }
+        })
+    ]
 }

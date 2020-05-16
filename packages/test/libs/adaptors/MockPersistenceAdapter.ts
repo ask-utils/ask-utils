cdd68276a398286bc65d8d8c4f31bb8404a88797
Hidetaka Okamoto <info@wp-kyoto.net>
2020-05-16 22:52:15 +0900

    fix ts build error

--- packages/test/libs/adaptors/MockPersistenceAdapter.ts
+++ packages/test/libs/adaptors/MockPersistenceAdapter.ts
@@ -14,10 +14,15 @@ export class MockPersistenceAdapter<T extends Attributes = Attributes> implement
         return this.attributes
     }
     public async saveAttributes (request: RequestEnvelope, attributes: Partial<T>): Promise<void> {
+        // just ignore ts build error
+        // eslint-disable-next-line no-self-assign
+        request.session = request.session
         this.attributes = attributes
     }
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     public async deleteAttributes (request: RequestEnvelope): Promise<void> {
+        // just ignore ts build error
+        // eslint-disable-next-line no-self-assign
+        request.session = request.session
         this.attributes = {}
     }
 }
